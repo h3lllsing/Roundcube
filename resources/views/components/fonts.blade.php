e242c7ac94dbf98f36feae5921c8373acd54dd93
cloudchapms-cmd <cloudchapms@gmail.com>
2026-07-13 22:54:36 +0500

    perf: add font-display swap to eliminate render-blocking font load

--- resources/views/components/fonts.blade.php
+++ resources/views/components/fonts.blade.php
@@ -1,3 +1,3 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
-<link rel="stylesheet" href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" media="print" onload="this.media='all'">
-<noscript><link rel="stylesheet" href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"></noscript>
+<link rel="stylesheet" href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600&display=swap" media="print" onload="this.media='all'">
+<noscript><link rel="stylesheet" href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600&display=swap"></noscript>
