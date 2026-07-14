c2c25187a550b41fd6c6f28d91a7bf6e4bb137bc
cloudchapms-cmd <cloudchapms@gmail.com>
2026-07-14 20:44:23 +0500

    chore: remove local debug and test artifacts from tracking

--- create_test_db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=127.0.0.1;port=3306', 'root', '');
-$pdo->exec('CREATE DATABASE IF NOT EXISTS opspilot_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci');
-echo "Database created/confirmed" . PHP_EOL;
--- test_mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    new PDO('mysql:host=127.0.0.1;port=3306', 'root', '');
-    echo "Connected OK";
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
